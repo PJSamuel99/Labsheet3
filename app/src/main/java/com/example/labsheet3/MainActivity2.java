fc641d2b4851d360a0e5ecafb68ce68f9ca49c92
PJSamuel99 <priscilla.samuel981@gmail.com>
2020-11-09 01:38:17 +0530

    second activity added

--- app/src/main/java/com/example/labsheet3/MainActivity2.java
+++ app/src/main/java/com/example/labsheet3/MainActivity2.java
@@ -3,17 +3,12 @@ package com.example.labsheet3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button btn;
-
-
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 }
