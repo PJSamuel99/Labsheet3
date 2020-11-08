630821897b355d1b66d0cad4ad0c9fef20924647
PJSamuel99 <priscilla.samuel981@gmail.com>
2020-11-09 01:08:34 +0530

    java variable added

--- app/src/main/java/com/example/labsheet3/MainActivity.java
+++ app/src/main/java/com/example/labsheet3/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.labsheet3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
